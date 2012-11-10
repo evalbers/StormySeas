6769eace85f8453f0f4314ca8797151cb1a8fe29
evalbers <sergio@nullrefs.com>
2012-11-10 00:49:27 -0600

    checkin #1

---
+++ Ev/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+
+namespace Ev.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "play the rpg \"Ev\" (html5 ultima5-esque fantasy)";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "play the rpg \"Ev\" (html5 ultima5-esque fantasy)";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "play the rpg \"Ev\" (html5 ultima5-esque fantasy)";
+
+            return View();
+        }
+    }
+}
